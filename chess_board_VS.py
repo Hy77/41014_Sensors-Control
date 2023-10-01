b12f989ee9ebd04af5942ff837d1e3fdca8dd6e6
Hy77 <74601064+Hy77@users.noreply.github.com>
2023-10-01 14:08:14 +1100

    Update chess_board_VS.py
    now can detect the black background frame(ROI), then detect the chess board inside of it.

--- chess_board_VS.py
+++ chess_board_VS.py
@@ -10,9 +10,40 @@ import numpy as np
 class ChessboardDetector:
     def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.image_callback)
+        self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.detect_roi)
+        self.prev_roi_area = 0  # initial previous ROI area
 
-    def image_callback(self, msg):
+    # detect the chess board within ROI
+    def detect_chess_board_within_roi(self, roi_image):
+        # narrow down the ROI area
+        h, w = roi_image.shape[:2]
+        roi_image = roi_image[70:h-70, 70:w-70]
+
+        # detect the black square in the reduced roi image
+        lower_black = np.array([0, 0, 0], dtype="uint8")
+        upper_black = np.array([130, 130, 130], dtype="uint8")
+        black_mask = cv2.inRange(roi_image, lower_black, upper_black)
+
+        # get shape
+        contours, _ = cv2.findContours(black_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # if found enough black areas, try to get a rectangle that surrounds them
+        if len(contours) > 0:
+            x_min = y_min = float('inf')
+            x_max = y_max = float('-inf')
+
+            for contour in contours:
+                x, y, w, h = cv2.boundingRect(contour)
+                x_min = min(x_min, x)
+                y_min = min(y_min, y)
+                x_max = max(x_max, x + w)
+                y_max = max(y_max, y + h)
+
+            # draw rectangle that surround all the black areas
+            cv2.rectangle(roi_image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+
+    # detect ROI (region of interest)
+    def detect_roi(self, msg):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError as e:
@@ -21,7 +52,7 @@ class ChessboardDetector:
 
         # set RGB
         lower_black = np.array([0, 0, 0], dtype="uint8")
-        upper_black = np.array([50, 50, 50], dtype="uint8")
+        upper_black = np.array([130, 130, 130], dtype="uint8")
 
         # color filter
         black_mask = cv2.inRange(cv_image, lower_black, upper_black)
@@ -32,13 +63,20 @@ class ChessboardDetector:
         # drawing bounding box
         for contour in contours:
             area = cv2.contourArea(contour)
-            if area > 5000:
+            if area > 10000:
                 approx = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
                 if len(approx) == 4:  # find quad
                     x, y, w, h = cv2.boundingRect(contour)
                     aspect_ratio = w / h
-                    if 0.5 < aspect_ratio < 1.5:  # check if this is a rec
-                        cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 0, 255), 2)  # 画出bounding box
+                    if 1.0 < aspect_ratio < 2.0:  # check if this is a rec
+                        new_roi_area = w * h
+                        if abs(new_roi_area - self.prev_roi_area) < self.prev_roi_area * 0.5:  # set -> 50%
+                            cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 0, 255), 2)  # draw bounding box
+                            # get ROI img from the screen (coordinates)
+                            roi_image = cv_image[y:y + h, x:x + w]
+                            # now detect the chess board within ROI
+                            self.detect_chess_board_within_roi(roi_image)
+                        self.prev_roi_area = new_roi_area  # update previous ROI's area
 
         # show img
         cv2.imshow("Image", cv_image)
